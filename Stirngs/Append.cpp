78397836576d1e286323d652b6319cd99c99482e
Shahper Shahin <74124339+ShahperShahin@users.noreply.github.com>
2023-02-17 23:28:16 +0530

    Add files via upload

---
+++ Stirngs/Append.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // string str;
+    // string s1="fam";
+    // string s2="ily";
+    
+    // //1st WAY
+    // s1.append(s2);
+    // cout<<s1<<"\n";
+
+    //2st WAY
+    // cout<<s1+s2<<"\n";
+
+
+//to access any index of string
+
+    // cout<<s1[1]<<"\n";
+
+    //to comapre length of two string 
+
+    string l1;
+    string l2;
+    cin>>l1;
+    cin>>l2;
+    cout<<l1.compare(l2);
+
+
+}
+
+
+
